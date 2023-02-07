f8cf069156a0568f358f7dff75b2e16bda5d71b9
LukaChabaud <luka.chabaud01@estudiant.upf.edu>
2023-02-07 16:02:32 +0100

    Connection and login

--- p2/MyClient.js
+++ p2/MyClient.js
@@ -3,10 +3,100 @@
 
 class MyClient 
 {
-    
+    socket = null
+    is_connected = null
+    room = { name:"", clients:[] }
+    clients = {}
+    num_clients = 0
+
+    user_id = 0
+    user_name = "Default"
+
+    on_connect = null; //when connected
+	on_ready = null; //when we have an ID from the server
+	on_message = null; //when somebody sends a message
+	on_close = null; //when the server closes
+	on_user_connected = null; //new user connected
+	on_user_disconnected = null; //user leaves
+	on_error = null; //when cannot connect
+
+    connect(room_name, user_name) {
+
+        room_name = room_name || ""
+        user_name = user_name || "Default"
+
+        if (this.socket){
+            this.socket.close()
+        }
+
+        this.clients = {}
+
+        let url = `ws://localhost:9016/${room_name}?username=${user_name}`
+        this.socket = new WebSocket(url)
+
+        this.socket.onopen = () => {
+            this.is_connected = true
+            this.room.name = room_name
+            console.log("Socket opened succesfully");
+
+            if (this.on_connect){
+                this.on_connect()
+            }
+        }
+
+        this.socket.onclose = () => {
+            this.is_connected = false 
+
+            if(this.on_close){
+                this.on_close()
+            }
+
+            this.socket = null
+            this.room = null
+            console.log("Socket has been closed");
+        }
+
+        this.socket.onmessage = (msg) => {
+            let message = JSON.parse(msg.data)
+            this.manageServerMessage(message)
+        }
+
+    }
+
+    manageServerMessage( message ) {
+        //TODO: manage different server events
+
+        switch (message.type) {
+            case "ID":
+                console.log("ID received");
+                this.user_id = message.userID
+                this.clients[this.user_id] = {id: this.user_id, name: this.user_name}
+
+                if(this.on_ready){
+                    this.on_ready()
+                }
+                break;
+        
+            case "LOGIN":
+                if(!this.clients[ message.userID ]) {
+                    this.clients[ message.userID ] = { id: message.userID, name: this.user_name };
+                    this.num_clients += 1;
+                }
+
+                if(message.content != this.user_id){
+                    console.log("User connected");
+                    if (this.on_user_connected) {
+                        this.on_user_connected(message.userID)
+                    }
+                }
+                break;
+
+            default:
+                break;
+        }
+    }
 }
 
-//Simple connection to check that it works
-//Imported into the index.html
+let client = new MyClient()
 
-let client = new WebSocket("ws://localhost:9016")
+client.connect("hello", "world")
--- p2/MyServer.js
+++ p2/MyServer.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const WebSocket = require('ws');
 const http = require('http');
+const url = require('url');
+const qs = require('querystring');
 
 class MyServer {
+
+    rooms = {}
+    clients = []
+    usernameToClient = {}
+    db = {}
+    lastID = 1
 
     constructor() {
 
@@ -15,7 +23,7 @@ class MyServer {
         
         this.wss.on("connection", this.on_connection.bind(this));
 
-        this.on_user_connected = null;
+        this.on_user_connected
         this.on_message = null
         this.on_user_disconnected = null
 
@@ -24,20 +32,77 @@ class MyServer {
 
 
     //Server connection
-    on_connection = (WebSocketClient, req) => {
+    on_connection (ws, req) {
         console.log("Connected");
-        WebSocketClient.send('{"connection": "ok"}');
 
-        WebSocketClient.on('message', (message) => {
+        //Parse the url
 
-            //for each websocket client
-            this.wss
-            .clients
-            .forEach( client => {
-                //send the client the current message
-                client.send(`{ "message" : ${message} }`);
-            });
-        });
+        let path = url.parse(req.url)
+        let user_name = qs.parse(path.query).username
+
+        let room_name = path.pathname
+        ws.room = room_name.substr(1, room_name.length)
+
+        
+        //Accept the new client and give it it's ID
+        this.get_new_id(ws)
+        
+        
+        //Add the new client 
+        if(this.rooms[ws.room] == null){
+            this.createRoom(ws.room)
+        }
+        this.usernameToClient[user_name] = ws
+        this.clients.push(ws)
+        this.rooms[ws.room].clients.push(ws)
+
+        //Tell everyone that a user has connected
+
+        let msg = {
+            username: user_name,
+            type: "LOGIN",
+            content: ws.id,
+            date: new Date()
+        }
+        this.sendToRoom(ws.room, JSON.stringify(msg))
+
+        //Configure on message callback
+        ws.on('message', this.on_message_received);
+    }
+
+    //Create a new room 
+    createRoom(room_name) {
+        this.rooms[room_name] = { clients: []}
+    }
+
+    get_new_id(ws) {
+        ws.id = this.lastID
+        this.lastID++
+
+        let msg = {
+            userID: ws.id,
+            content: "", 
+            type: "ID",
+            date: new Date()
+        }
+        ws.send(JSON.stringify(msg))
+        console.log("Sent the new id " + ws.id);
+        return ws.id
+    }
+
+    
+    //Message received
+    on_message_received(message) {
+        
+        message = JSON.parse(message)
+
+        switch (message.type) {
+            case "":
+                break;
+        
+            default:
+                break;
+        }
     }
 
     //User Connection
@@ -57,6 +122,28 @@ class MyServer {
         this.server.listen(port, () => {
             console.log("Server listening on port " + port);
         });
+    }
+
+    //Helper function that broadcasts a message to all the clients of a room (or to the targeted ID's)
+    sendToRoom (room_name, msg, target){
+
+        if ( msg === undefined){
+            return
+        }
+        let room = this.rooms[room_name]
+        if ( !room ){
+            return
+        }
+        console.log(room.clients.length);
+        for(let client of room.clients){
+            if (target && !(client.user_id in target)) {
+                continue
+            }
+            if(client.readyState != WebSocket.OPEN){
+			    continue;
+            }
+            client.send(msg)
+        }
     }
 }
 
